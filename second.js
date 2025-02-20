80520a242ff77bfe5ce668238f7c473014bd35a2
Aswarth <212g1a0403@gmail.com>
2025-02-20 18:34:47 +0530

    answer

--- second.js
+++ second.js
@@ -1,2 +1,11 @@
-console.log("kireeti")
-console.log("hi")
+
+function index(arr,N,M){
+    for(let i=0;i<N;i++){
+        let ans=""
+        for(let j=0;j<M;j++){
+            ans+=(i+j)+" "
+        }
+        console.log(ans)
+    }
+}
+index([[1,2],[3,4],[5,6]],3,2)
