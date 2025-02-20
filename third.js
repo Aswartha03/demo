8846882ad3fae4cd0f0ed28bc83b6ff64fb02bec
Aswarth <212g1a0403@gmail.com>
2025-02-20 18:41:03 +0530

    fourth answer

--- third.js
+++ third.js
@@ -1,1 +1,15 @@
-console.log("masai")
+function pattern(n){
+    for(let i=0;i<n;i++){
+        let bag=""
+        for(let j=0;j<n;j++){
+            if(i==0 || j==0 || i==n-1){
+                bag+="*"+" "
+            }
+            else{
+                bag+=" "
+            }
+        }
+        console.log(bag)
+    }
+}
+pattern(5)
